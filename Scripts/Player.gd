70d8f9a374573a8f4068b7ddd0ee70f54073f5ad
June Turner <destroyermce@gmail.com>
2022-12-10 12:12:20 -0800

    some implementation of crouching

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -33,20 +33,38 @@ const CeilingVarJumpGrace = .05
 const UpwardCornerCorrection = 4
 const WallSpeedRetentionTime = .06
 
+const StandardHitBox = Rect2(0, -6, 4, 6)
+const StandardHurtBox = Rect2(0, -7, 4, 5)
+const DuckHitBox = Rect2(0, -3, 4, 3)
+const DuckHurtBox = Rect2(0, -4, 4, 2)
+
 var velocity = Vector2.ZERO
 var subpixel = Vector2.ZERO
 
-var jump_no_grav = 0
-var jump_buffer = 0
+var crouched = false
+
+var jumpNoGrav = 0
+var jumpBuffer = 0
 
 var fastJump = false
+var cyoteTime = 0
 
+var spawnPos
+
+func set_hitbox(collider : CollisionShape2D, rect : Rect2):
+	
+	collider.position = rect.position
+	if collider.shape is RectangleShape2D:
+		collider.shape.extents = rect.size
+	else:
+		print("CollisionShape does not have a RectangleShape2D!")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	#get_tree().get_root().size = Vector2(320, 180)
-
+	
+	spawnPos = position
+	
+	$Camera.zoom = Vector2(320, 180) / get_tree().get_root().size
 
 func _physics_process(delta):
 	
@@ -59,30 +77,37 @@ func _physics_process(delta):
 		RunMult = AirMult
 	
 	var move = Vector2(Input.get_axis("move_left","move_right"), Input.get_axis("move_up", "move_down"))
-	if move.x == 0:
-		velocity.x = move_toward(velocity.x, 0, delta * RunReduce * RunMult)
-	elif velocity.x * move.x < MaxRun:
-		velocity.x = move_toward(velocity.x, move.x * MaxRun, delta * RunAccel * RunMult)
-	else:
-		velocity.x = move_toward(velocity.x, move.x * MaxRun, delta * RunReduce * RunMult)
+	if !crouched or !grounded:
+		if move.x == 0:
+			velocity.x = move_toward(velocity.x, 0, delta * RunReduce * RunMult)
+		elif velocity.x * move.x < MaxRun:
+			velocity.x = move_toward(velocity.x, move.x * MaxRun, delta * RunAccel * RunMult)
+		else:
+			velocity.x = move_toward(velocity.x, move.x * MaxRun, delta * RunReduce * RunMult)
 	
 	if Input.is_action_just_pressed("jump"):
-		jump_buffer = 0.1
+		jumpBuffer = 0.1
 	if Input.is_action_just_released("jump"):
-		jump_buffer = 0
-	
+		jumpBuffer = 0
+		
+	if Input.is_action_just_pressed("reset"):
+		position = spawnPos
 	
 	if grounded:
 		velocity.y = 1
-		if jump_buffer > 0:
-			jump_buffer = 0
-			velocity.y = JumpSpeed
-			velocity.x += JumpHBoost * move.x
-			jump_no_grav = 0.2
-		else:
-			jump_buffer = move_toward(jump_buffer, 0, delta)
-	elif jump_no_grav == 0 or !Input.is_action_pressed("jump"):
-		jump_no_grav = 0
+		cyoteTime = JumpGraceTime
+		if move.y > 0 and !crouched:
+			crouched = true
+			$Sprite.animation = "duck"
+			set_hitbox($Hitbox, DuckHitBox)
+			set_hitbox($Hurtbox/Collider, DuckHurtBox)
+		elif move.y <= 0 and crouched:
+			crouched = false
+			set_hitbox($Hitbox, StandardHitBox)
+			set_hitbox($Hurtbox/Collider, StandardHurtBox)
+			
+	elif jumpNoGrav == 0 or !Input.is_action_pressed("jump"):
+		jumpNoGrav = 0
 		if move.y > 0 and velocity.y >= 160:
 			velocity.y = move_toward(velocity.y, FastMaxFall, FastMaxAccel * delta)
 		elif velocity.y >= 160:
@@ -92,26 +117,36 @@ func _physics_process(delta):
 		else:
 			velocity.y = move_toward(velocity.y, MaxFall, Gravity * delta)
 	else:
-		jump_no_grav = move_toward(jump_no_grav, 0, delta)
+		jumpNoGrav = move_toward(jumpNoGrav, 0, delta)
+	
+	if cyoteTime > 0 and jumpBuffer > 0:
+			jumpBuffer = 0
+			velocity.y = JumpSpeed
+			velocity.x += JumpHBoost * move.x
+			jumpNoGrav = 0.2
+	
+	jumpBuffer = move_toward(jumpBuffer, 0, delta)
+	cyoteTime = move_toward(cyoteTime, 0, delta)
 	
 	if move.x != 0:
 		if move.x < 0:
 			$Sprite.flip_h = true
 		else:
 			$Sprite.flip_h = false
-		
-	if velocity.x != 0 and grounded:
-		$Sprite.animation = "walk"
-	elif grounded:
-		$Sprite.animation = "idle"
-	elif fastJump or abs(velocity.x) > MaxRun:
-		if $Sprite.animation != "jumpFast":
-			$Sprite.animation = "jumpFast"
-			$Sprite.frame = 0
-	else:
-		if $Sprite.animation != "jumpSlow":
-			$Sprite.animation = "jumpSlow"
-			$Sprite.frame = 0
+	
+	if !crouched:
+		if velocity.x != 0 and grounded:
+			$Sprite.animation = "walk"
+		elif grounded:
+			$Sprite.animation = "idle"
+		elif fastJump or abs(velocity.x) > MaxRun:
+			if $Sprite.animation != "jumpFast":
+				$Sprite.animation = "jumpFast"
+				$Sprite.frame = 0
+		else:
+			if $Sprite.animation != "jumpSlow":
+				$Sprite.animation = "jumpSlow"
+				$Sprite.frame = 0
 		
 	
 	position += subpixel
